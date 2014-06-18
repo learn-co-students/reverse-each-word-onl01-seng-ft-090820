dbf766a08988a60f67a4a0b72a366a180f11161f
ahimmelstoss <ahimmelstoss@gmail.com>
2014-06-18 18:19:34 -0400

    solution

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  sentence.split(" ").collect {|word| word.reverse}.join(" ")
+end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split(" ").reverse.join(" ")
+# end

1951e8aef09cef9d4274f115c48c683578b9ddcb
deniznida <denizunat@hotmail.com>
2015-08-10 17:57:51 -0400

    adds tutorial

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,3 @@
 def reverse_each_word(sentence)
-  sentence.split(" ").collect {|word| word.reverse}.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
-
-# Without iterating:
-
-# def reverse_each_word(sentence)
-#  sentence.reverse.split(" ").reverse.join(" ")
-# end
